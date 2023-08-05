357655eb9098f22923e67dd4cf567f00eb34cbb9
swstbe <1637373955@qq.com>
2023-08-05 20:27:52 +0800

    add code for the first time

---
+++ build/grid_path_searcher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/swstbe/SL_Planning/hw2/src/grid_path_searcher
+# Build directory: /home/swstbe/SL_Planning/hw2/build/grid_path_searcher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
